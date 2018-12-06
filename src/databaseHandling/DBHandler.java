4df4c2449444ae4ffce2fe212cb200d533f3df62
Pontus <Pontus.fredriksson1@gmail.com>
2018-12-06 11:51:09 +0100

    Continued on DBHandler.java

--- src/databaseHandling/DBHandler.java
+++ src/databaseHandling/DBHandler.java
@@ -1,19 +1,43 @@
+package databaseHandling;
+
+import ContactHandling.Contact;
+
 import java.sql.*;
 
-Class DBHandler{
-    Private DBHandler DBHandlerInstance=new DBHandler;
+public class DbHandler {
+    private DbHandler DbHandlerInstance =new DbHandler();
     String searchString;
     int pagenumber;
     int pageAmount;
     int hitsPerpage;
     int hitAmount;
 
-    public getInstance(){
+    private DbHandler(){}
+
+    public DbHandler getInstance(){
+        return DbHandlerInstance;
         }
 
 
-    add(Connection,contact);
-    delete(Connection,contact);
-    update(Connection,contact);
-    searchPage(Connection,contact);
+    public void add(Connection connection, Contact contact){
+        if (AddDb.callDb(connection, contact)==true){
+
+        }
+    }
+    public void delete(Connection connection,Contact contact){
+        if (DeleteDb.callDb(connection, contact)==true){
+
+        }
+    }
+    public void update(Connection connection,Contact contact){
+        if (UpdateDb.callDb(connection, contact)==true){
+
+        }
+    }
+
+    public void searchPage(Connection connection,Contact contact){
+        if (SearchPage.callDb(/* MASSA IN HÄR!! */)==true){
+            contacthandler.getInstance.createFromString(SearchPage.getInstance.Result);
+        }
+    }
 }
