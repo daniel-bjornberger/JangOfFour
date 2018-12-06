222e36d1f383b4d2560eef1e23d2b8b55a7d1d6d
Pontus <Pontus.fredriksson1@gmail.com>
2018-12-06 11:08:40 +0100

    Started on DBHandler.java

---
+++ src/databaseHandling/DBHandler.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+Class DBHandler{
+    Private DBHandler DBHandlerInstance=new DBHandler;
+    String searchString;
+    int pagenumber;
+    int pageAmount;
+    int hitsPerpage;
+    int hitAmount;
+
+    public getInstance(){
+        }
+
+
+    add(Connection,contact);
+    delete(Connection,contact);
+    update(Connection,contact);
+    searchPage(Connection,contact);
+}
