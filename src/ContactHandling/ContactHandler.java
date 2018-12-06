2d443f8d6ed0d1578d00b3f6357a7108301c9c14
Bob-Utveckling <development@bamshad.se>
2018-12-06 22:07:49 +0100

    give clearer structure to ContactHandler file

--- src/ContactHandling/ContactHandler.java
+++ src/ContactHandling/ContactHandler.java
@@ -6,28 +6,32 @@ import java.util.List;
 import java.sql.ResultSet;
 
 public class ContactHandler {
-
+    List<Contact> contactList = new ArrayList<>();
     private ContactHandler ContactHandlerInstance = new ContactHandler();
-    List<Contact> contactList;
-
-
     private ContactHandler() {}
 
     public ContactHandler getInstance() {
         return ContactHandlerInstance;
     }
 
-
     public String createFromString (java.sql.ResultSet sqlResultSet) {
-
+        //given: SearchPageDb.getInstance.Result
+        //create one or more contact from that
+        System.out.println("Working with: " +
+                sqlResultSet);
+        return ("the created contact");
     }
 
-    //public ContactHandler () {
-    //}
+    public void add (ContactHandling.Contact givenContact) {
+        //add a given contact detail to contactList
+        //has an error , fix:
+        contactList.add(new Contact(givenContact));
+    }
 
+    public void clear (Contact givenContact) {
+        //remove a contact given contact
+        //something like:
+        // contactList.remove(givenContact);
+    }
 
-    //public void add (Contact givenContact) {
-    //}
-
-    //public void clear (Contact givenContact) {}
 }
