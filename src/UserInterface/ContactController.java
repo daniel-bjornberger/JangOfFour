c7b9a610d01918756dd88c4b250ec9e46669767f
Daniel Björnberger <daniel.bjornberger@gmail.com>
2018-12-10 14:42:30 +0100

    Add fx:id to the two fxml files and declare the corresponding variables in the two Controller classes

--- src/UserInterface/ContactController.java
+++ src/UserInterface/ContactController.java
@@ -1,4 +1,33 @@
 package UserInterface;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 public class ContactController {
+
+
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField phoneField;
+    @FXML
+    private TextField addressField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private Button okButton;
+    @FXML
+    private Button cancelButton;
+
+
+
+
+
+
+
+
 }
--- src/UserInterface/ListOfContactsController.java
+++ src/UserInterface/ListOfContactsController.java
@@ -1,4 +1,39 @@
 package UserInterface;
 
+import ContactHandling.Contact;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+
 public class ListOfContactsController {
+
+
+    @FXML
+    private TextField searchField;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private Button listAllContactsButton;
+    @FXML
+    private Button addNewContactButton;
+    @FXML
+    private TableView<Contact> tableView;
+    @FXML
+    private TableColumn<Contact, String> firstNameColumn;
+    @FXML
+    private TableColumn<Contact, String> lastNameColumn;
+    @FXML
+    private TableColumn<Contact, String> phoneColumn;
+    @FXML
+    private TableColumn<Contact, String> addressColumn;
+    @FXML
+    private TableColumn<Contact, String> emailColumn;
+    @FXML
+    private Pagination pagination;
+
+
+
+
+
+
 }
