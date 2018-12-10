b79ec53402cc6fba4653dbc35827fcc4e9f6fcee
Daniel Björnberger <daniel.bjornberger@gmail.com>
2018-12-10 20:33:21 +0100

    Change name of the class Main, and simplify the code. Add the method initialize to class ListOfContactsController. Add Update and Delete buttons to fxml file ListOfContactsView and change the font size.

--- src/UserInterface/ContactListMain.java
+++ src/UserInterface/ContactListMain.java
@@ -6,22 +6,36 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class ContactListMain extends Application {
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage stage) throws Exception{
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("ListOfContactsView.fxml"));
+        /*FXMLLoader loader = new FXMLLoader(getClass().getResource("ListOfContactsView.fxml"));
         Parent root = loader.load();
 
         ListOfContactsController listOfContactsController = loader.getController();
-        //listOfContactsController.init();
+        listOfContactsController.init();
         //listOfContactsController.setStage(primaryStage);
 
         primaryStage.setTitle("Jang Of Four Contact List");
         primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        primaryStage.show();*/
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("ListOfContactsView.fxml"));
+
+        stage.setTitle("Jang Of Four Contact List");
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+
+
+
+
 
     }
 
