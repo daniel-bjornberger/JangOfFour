748a1d87b0e97584a5219e9d187e140e1048a8b8
Pontus <Pontus.fredriksson1@gmail.com>
2018-12-11 15:56:27 +0100

    add selectedContact in Model
    We save the info here so both controllerclasses can handle the same
    object. this is a clone of the inputed one, not the same!

--- src/userinterface/Model.java
+++ src/userinterface/Model.java
@@ -1,15 +1,24 @@
 package userinterface;
 
+import contacthandling.Contact;
 import contacthandling.ContactHandler;
 import databasehandling.DbHandler;
 
 public class Model {
 
  private DbHandler dbHandler=DbHandler.getInstance();
+ private ContactHandler contactHandler=ContactHandler.getInstance();
+ private Contact selectedContact;
 
-    private ContactHandler contactHandler=ContactHandler.getInstance();
+    public void setSelectedContact(Contact selectedContact) {
+        this.selectedContact = selectedContact;
+    }
 
- public DbHandler getDbHandler() {
+    public Contact getSelectedContact(){
+        return selectedContact;
+    }
+
+    public DbHandler getDbHandler() {
         return dbHandler;
     }
 
