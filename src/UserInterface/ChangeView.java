e8ffbc0536d1db1f645e4e492b745ee68f28d113
joeliths <your>
2018-12-11 15:38:28 +0100

    Changed methos ChangeView

--- src/UserInterface/ChangeView.java
+++ src/UserInterface/ChangeView.java
@@ -13,12 +13,17 @@ public class ChangeView {
 
     public  void newWindow(String path) {
 
-        Parent root;
+
         try {
-            root = FXMLLoader.load(getClass().getClassLoader().getResource("/ContactView.fxml"));
+
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/ContactView.fxml"));
+            Parent content = loader.load();
+            //root = FXMLLoader.load(getClass().getClassLoader().getResource("ContactView.fxml"),r);
             Stage stage = new Stage();
             stage.setTitle("My New Stage Title");
-            stage.setScene(new Scene(root, 450, 450));
+            stage.setScene(new Scene(content, 1536, 864));
             stage.show();
             // Hide this current window (if this is what you want)
             //((Node) (event.getSource())).getScene().getWindow().hide();
--- src/UserInterface/ListOfContactsController.java
+++ src/UserInterface/ListOfContactsController.java
@@ -8,9 +8,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -130,8 +134,22 @@ public class ListOfContactsController {
     }
 
     public void actionAddNewContact(ActionEvent event) {
-        ChangeView changeView=new ChangeView();
-        changeView.newWindow("ContactView.fxml");
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("ContactView.fxml"));
+            /*
+             * if "fx:controller" is not set in fxml
+             fxmlLoader.setController(NewWindowController);
+             */
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Stage stage = new Stage();
+            stage.setTitle("New Window");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("e´rror");
+        }
 
     }
 }
