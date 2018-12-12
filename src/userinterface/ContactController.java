6c77cd105565daba59a56589b03faae1135a6775
Daniel Björnberger <daniel.bjornberger@gmail.com>
2018-12-12 01:47:59 +0100

    Add onAction-event to the Cancel button in the fxml file ContactView and the corresponding method in the class ContactController, and add the functionality to change scene to the class ListOfContactsController

--- src/userinterface/ContactController.java
+++ src/userinterface/ContactController.java
@@ -4,8 +4,15 @@ import contacthandling.Contact;
 import databasehandling.DbHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class ContactController {
@@ -33,4 +40,25 @@ public class ContactController {
         //firstNameField.getText()
 
     }
+
+    public void actionCancel(ActionEvent event) {
+
+        Parent parent = null;
+
+        try {
+            parent = FXMLLoader.load(getClass().getResource("ListOfContactsView.fxml"));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        Scene scene = new Scene(parent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(scene);
+        window.show();
+
+    }
+
+
 }
--- src/userinterface/ListOfContactsController.java
+++ src/userinterface/ListOfContactsController.java
@@ -7,6 +7,8 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -154,7 +156,7 @@ public class ListOfContactsController {
 
         //tableView.setItems(contacts);
         model.getContactHandler().addListener((c) -> updateView());
-//tableView.setItems((ObservableList<Contact>) model.getContactHandler().getContactList())
+        //tableView.setItems((ObservableList<Contact>) model.getContactHandler().getContactList())
     }
 
     public void updateView() {
@@ -168,7 +170,27 @@ public class ListOfContactsController {
 
     public void actionAddNewContact(ActionEvent event) {
 
+        /*
         ChangeView changeView = new ChangeView();
         changeView.newWindow("ContactView.fxml");
+        */
+
+
+        Parent parent = null;
+
+        try {
+            parent = FXMLLoader.load(getClass().getResource("ContactView.fxml"));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        Scene scene = new Scene(parent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(scene);
+        window.show();
+
+
     }
 }
