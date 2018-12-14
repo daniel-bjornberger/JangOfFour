8720e76b339d6ff12ca64667ac63d9359b487666
Daniel Björnberger <daniel.bjornberger@gmail.com>
2018-12-14 09:43:45 +0100

    Tidy up the class ContactListMain by removing unused lines of code

--- src/userinterface/ContactListMain.java
+++ src/userinterface/ContactListMain.java
@@ -7,24 +7,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class ContactListMain extends Application {
 
 
     @Override
     public void start(Stage stage) throws Exception{
-        DBValidator dbValidator=new DBValidator();
 
-        /*FXMLLoader loader = new FXMLLoader(getClass().getResource("ListOfContactsView.fxml"));
-        Parent root = loader.load();
-
-        ListOfContactsController listOfContactsController = loader.getController();
-        listOfContactsController.init();
-        //listOfContactsController.setStage(primaryStage);
-
-        primaryStage.setTitle("Jang Of Four Contact List");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();*/
-
+        DBValidator dbValidator = new DBValidator();
 
         Parent root = FXMLLoader.load(getClass().getResource("ListOfContactsView.fxml"));
 
@@ -35,15 +25,15 @@ public class ContactListMain extends Application {
         stage.setScene(scene);
         stage.show();
 
-
-
-
-
     }
+
 
 
     public static void main(String[] args) {
+
         launch(args);
+
     }
+
 
 }
