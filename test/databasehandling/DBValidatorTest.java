e74d37f43370a5bf6308b22c5b768f2bfab8904c
joeliths <your>
2018-12-17 15:04:25 +0100

    add JavaDoc to TestClasses

--- test/databasehandling/DBValidatorTest.java
+++ test/databasehandling/DBValidatorTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**Test if when create an instance of DbValidator a connection is created. Test if method hasConnection return true when instance of DBvalidator is created.
+ * @author Joel Gunnarsson
+ */
 class DBValidatorTest {
 
     @Test
