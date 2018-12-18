037816ee3ff44c85205974af4325e04ff13cca89
Daniel Björnberger <daniel.bjornberger@gmail.com>
2018-12-18 15:41:56 +0100

    Add Javadoc comments to the class ContactListMain

--- src/userinterface/ContactListMain.java
+++ src/userinterface/ContactListMain.java
@@ -8,16 +8,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
+/** Class that extends javafx.application.Application, the class is used to launch JavaFX.
+ * @author Daniel Björnberger
+ */
 public class ContactListMain extends Application {
 
 
+    /** The method start is called when the JavaFX application is started. The method sets up a scene/view within the Stage.
+     * The scene/view is defined by the fxml file ListOfContactsView.
+     * @param stage The object stage is where the the visual parts of the JavaFX application will be displayed,
+     *              the stage is created by the JavaFX runtime.
+     * @throws Exception The method FXMLLoader.load may throw an IOException.
+     */
     @Override
-    public void start(Stage stage) throws Exception{
+    public void start(Stage stage) throws Exception {
 
         DBValidator dbValidator = new DBValidator();
 
         Parent root = FXMLLoader.load(getClass().getResource("ListOfContactsView.fxml"));
-        //Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("ListOfContactsView.fxml"));
+
         stage.setTitle("Jang Of Four Contact List");
 
         Scene scene = new Scene(root);
@@ -28,7 +37,9 @@ public class ContactListMain extends Application {
     }
 
 
-
+    /** The main method calls the static method launch in the Application class, which launches JavaFX runtime and the JavaFX application.
+     * @param args Parameters to the Java main method, not used.
+     */
     public static void main(String[] args) {
 
         launch(args);
