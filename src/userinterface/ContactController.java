769d8087b6ce302144c14eef8ad009b5ae94cb77
Daniel Björnberger <daniel.bjornberger@gmail.com>
2018-12-18 11:25:28 +0100

    Update the requirements for enabling the save button, check length for first name, last name, phone number and address, and check that the email contains the characters @ (at sign) and . (dot).

--- src/userinterface/ContactController.java
+++ src/userinterface/ContactController.java
@@ -3,11 +3,13 @@ package userinterface;
 import contacthandling.Contact;
 import databasehandling.DbHandler;
 import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import java.util.regex.Pattern;
 
 
 /** Class that handles the JavaFX view that is defined by the fxml file ContactView,
@@ -15,7 +17,6 @@ import javafx.scene.control.TextField;
  * @author Daniel Björnberger
  */
 public class ContactController {
-
 
     /** An instance of the singleton class Model. */
     Model model = Model.getInstance();
@@ -29,25 +30,32 @@ public class ContactController {
      * or an existing contact shall be updated.  */
     @FXML
     private Label contactHeading;
+
     /** Text field used for input/update of the first name of the contact. */
     @FXML
     private TextField firstNameField;
+
     /** Text field used for input/update of the last name of the contact. */
     @FXML
     private TextField lastNameField;
+
     /** Text field used for input/update of the phone number of the contact. */
     @FXML
     private TextField phoneField;
+
     /** Text field used for input/update of the address of the contact. */
     @FXML
     private TextField addressField;
+
     /** Text field used for input/update of the email of the contact. */
     @FXML
     private TextField emailField;
+
     /** Button used to confirm the addition of a new contact / update of an existing contact,
      * and return to the ListOfContacts view. */
     @FXML
     private Button saveButton;
+
     /** Button used to abort addition of a new contact / update of an existing contact,
      * and return to the ListOfContacts view. */
     @FXML
@@ -56,16 +64,20 @@ public class ContactController {
 
     /** The method initialize is automatically called when the ContactController view is shown,
      * in this method the save button disable property is bound to the text properties of the five text fields
-     * in order to make sure that the save button can't be clicked when one or more of the text fields are empty.
+     * in order to make sure that the save button can't be clicked when one or more of the text fields have incorrect values.
      * Also, if an existing contact has been selected for an update, the five text fields are filled with the
      * corresponding contact data. */
     public void initialize() {
 
-        saveButton.disableProperty().bind((Bindings.isEmpty(firstNameField.textProperty()))
-                .or(Bindings.isEmpty(lastNameField.textProperty()))
-                .or(Bindings.isEmpty(phoneField.textProperty()))
-                .or(Bindings.isEmpty(addressField.textProperty()))
-                .or(Bindings.isEmpty(emailField.textProperty())));
+        Pattern atSign = Pattern.compile(".*@.*");
+        Pattern period = Pattern.compile(".*[.].*");
+
+        saveButton.disableProperty().bind((firstNameField.textProperty().length().lessThan(2))
+                .or(lastNameField.textProperty().length().lessThan(2))
+                .or(phoneField.textProperty().length().lessThan(9))
+                .or(addressField.textProperty().length().lessThan(5))
+                .or(patternTextFieldBinding(emailField, period))
+                .or(patternTextFieldBinding(emailField, atSign)));
 
         selectedContact = model.getSelectedContact();
 
@@ -87,6 +99,22 @@ public class ContactController {
         }
 
     }
+
+
+
+    /** The method patternTextFieldBinding creates a BooleanBinding between a TextField and a Pattern.
+     * @param textField The TextField for which the text shall be matched to a Pattern.
+     * @param pattern The Pattern that shall be fulfilled by the text in the TextField.
+     * @return A BooleanBinding between the text in the TextField and a defined Pattern.
+     */
+    private BooleanBinding patternTextFieldBinding(TextField textField, Pattern pattern) {
+
+        BooleanBinding binding = Bindings.createBooleanBinding(() ->
+                !pattern.matcher(textField.getText()).matches(), textField.textProperty());
+        return binding;
+
+    }
+
 
 
     /** The method actionSave is used to trigger the update of an existing contact in the database, or the addition
@@ -120,6 +148,7 @@ public class ContactController {
         changeView.newView("ListOfContactsView.fxml", event);
 
     }
+
 
 
     /** The method actionCancel is used to switch back to the ListOfContactsView without doing any modification
