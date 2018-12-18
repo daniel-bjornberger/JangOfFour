2349bc5daedc110e52a5a59fa40382501a17f623
Daniel Björnberger <daniel.bjornberger@gmail.com>
2018-12-18 17:06:54 +0100

    Update test callDbAddContact in test class AddDbTest to testCallDbAddContactSuccess, and add new test testCallDbAddContactFail

--- test/databasehandling/AddDbTest.java
+++ test/databasehandling/AddDbTest.java
@@ -2,9 +2,8 @@ package databasehandling;
 
 import contacthandling.Contact;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,10 +14,36 @@ class AddDbTest {
         DBValidator.closeConnection();
     }
 
+
     @Test
-    void callDbAddContact() throws Exception {
-        DBValidator dbValidator=new DBValidator();
-        assertTrue(AddDb.callDb(DBValidator.getCon(),new Contact(DbHandler.getInstance().generateID(),"Joel","Gunnarsson","098","Ringvägen","joel@hotmail.com")));
+    void testCallDbAddContactSuccess() throws Exception {
+
+        DBValidator dbValidator = new DBValidator();
+
+        Contact newContact = new Contact(-34567, "Joel","Gunnarsson","098","Ringvägen","joel@hotmail.com");
+
+        assertTrue(AddDb.callDb(DBValidator.getCon(), newContact));
+
+        System.out.println("HEJ!");
+
+        if(DeleteDb.callDb(DBValidator.getCon(), newContact)){
+            System.out.println("Delete ok!");
+        }
+
+    }
+
+
+    @Test
+    void testCallDbAddContactFail() throws Exception {
+
+        Contact newContact = new Contact(-34567, "Joel","Gunnarsson","098","Ringvägen","joel@hotmail.com");
+
+        AddDb.callDb(DBValidator.getCon(), newContact);
+
+        assertFalse(AddDb.callDb(DBValidator.getCon(), newContact));
+
+        DeleteDb.callDb(DBValidator.getCon(), newContact);
+
     }
 
 }
