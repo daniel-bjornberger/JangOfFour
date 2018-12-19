a717b8947acb97866c589eba60e447c5bbb96a53
Bob-Utveckling <development@bamshad.se>
2018-12-19 11:29:18 +0100

    Update test class ContactCreateTest

--- test/contacthandling/ContactCreateTest.java
+++ test/contacthandling/ContactCreateTest.java
@@ -15,8 +15,9 @@ class ContactCreateTest {
     public void createAContactWithCommonValuesForAContact() {
         try {
             new Contact(999, "Bob", "L.","0736262671", "Address1","bobfripostorg");
-            fail ("Exception was expected for mejl address...");
+
         } catch (Exception e) {
+            fail ("Exception was expected for mejl address...");
             System.err.println("Exception: " + e);
         }
     }
@@ -25,18 +26,20 @@ class ContactCreateTest {
     public void makeSureContactFirstNameIsNotEmpty() {
         try {
             new Contact(999, "", "L.","0736262671", "Address1","bob@fripost.org");
-            fail("Exception was expected for empty first name");
+
         } catch (Exception e) {
+            fail("Exception was expected for empty first name");
             System.err.println("Exception: " + e);
         }
     }
 
     @Test
-    void makeSureContactLastNameNotNull() {
+    void makeSureContactLastNameNotEmpty() {
         try {
-            new Contact(999, "Bob", "","0736262671", "Address1","bob@fripost.org");
-            fail("Exception was expected for empty last name");
+            new Contact(999, "Bob", "Gunnarsson","0736262671", "Address1","bob@fripost.org");
+
         } catch (Exception e) {
+            fail("Exception was expected for empty last name");
             System.err.println("Exception: " + e);
         }
     }
@@ -44,9 +47,10 @@ class ContactCreateTest {
     @Test
     void seeThatPhoneNumberIsInCorrectFormat() {
         try {
-            new Contact(999, "Bob", "","071", "Address1","bob@fripost.org");
-            fail("Exception was expected for short phone numbers");
+            new Contact(999, "Bob", "Lotfabadi","071234567", "Address1","bob@fripost.org");
+
         } catch (Exception e) {
+            fail("Exception was expected for short phone numbers");
             System.err.println("Exception: " + e);
         }
     }
