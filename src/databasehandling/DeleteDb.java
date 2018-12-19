c3b3a0bf4eb3af48af2704d301c38d1155aed069
Daniel Björnberger <daniel.bjornberger@gmail.com>
2018-12-19 12:51:40 +0100

    Clean up DeleteDb and UpdateDb, by removing prints to stdout and comments

--- src/databasehandling/DeleteDb.java
+++ src/databasehandling/DeleteDb.java
@@ -8,15 +8,13 @@ public abstract class DeleteDb {
     Given the database connection and a given contact detail object,
     delete this contact from table.
     Base the deletion on the contact's Id.
-    @author Joel
+    @author Joel Gunnarsson
     */
     public static boolean callDb(Connection connection,Contact contact) {
 
         int rowCount = -1;
 
         String SQL = "DELETE FROM contacts WHERE id = " + contact.getId() + ";";
-
-        System.out.println(SQL);        // TODO Check of SQL statement, shall eventually be deleted.
 
         if (connection != null) {
 
@@ -25,22 +23,15 @@ public abstract class DeleteDb {
 
                 rowCount = stm.executeUpdate(SQL);
 
-                stm.close();                // TODO Is this correct? What about 'finally' below?
+                stm.close();
             }
             catch (SQLException e) {
                 System.err.println(e.getMessage());
             }
 
-
-
-            /*finally {
-
-            }*/
-
-
         }
 
-        return rowCount == 1;                // TODO Check how the return statement shall be written. Is rowCount used correctly?
+        return rowCount == 1;
 
     }
 
--- src/databasehandling/UpdateDb.java
+++ src/databasehandling/UpdateDb.java
@@ -8,7 +8,7 @@ import java.sql.*;
  * Given the database connection and a given contact detail object,
  * update the contact with new contact detail information.
  * Return true if succeeded, false otherwise
- * @author Joel
+ * @author Joel Gunnarsson
  */
 public abstract class UpdateDb {
 
@@ -25,7 +25,7 @@ public abstract class UpdateDb {
                         "',address='"+contact.getAddress()+
                         "',email='"+contact.getEmail()+
                         "' WHERE id="+contact.getId()+";";
-                System.out.println(query); // Säkra att du skrivit rätt, tas bort senare.
+
                 stm = connection.createStatement();
                 rows = stm.executeUpdate(query);
 
@@ -40,7 +40,7 @@ public abstract class UpdateDb {
             }
 
         }
-        // TODO change to 1?
+
         if (rows <= 0) {
             return false;
         } else {
