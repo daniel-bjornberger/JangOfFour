632649f9cd93e20a47f3d711c1024be688298bee
Daniel Björnberger <daniel.bjornberger@gmail.com>
2018-12-19 11:01:45 +0100

    Start writing Javadoc comments for the test class AddDbTest

--- test/databasehandling/AddDbTest.java
+++ test/databasehandling/AddDbTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-/** Test class for
- *
+/** Test class for the AddDb class.
+ * @author Daniel Björnberger
  */
 class AddDbTest {
 
